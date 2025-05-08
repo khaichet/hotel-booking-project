52c5fb25044ab9664393fafc8da29405df0a010b
khaichet <khaichet1708@gmail.com>
2025-05-08 19:50:35 +0700

    form date booking

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { CalendarOutlined, SearchOutlined } from '@ant-design/icons'
+import { cities } from '../assets/assets'
 
 const Hero = () => {
     return (
@@ -6,6 +8,48 @@ const Hero = () => {
             <p className='bg-[#49b9ff]/50 px-3,5 py-1 rounded-full mt-20'>The Ultimate Hotel Experience</p>
             <h1 className=' font-playfair text-2xl md:text-5xl md:tex-[56px] ,md:leading-[56px] font-bold md:font-extrabold max-w-xl'>Discover Your Perfect Gateway Destination </h1>
             <p className='max-w-130 mt-2 text-sm md:text-base'>Unparalleled luxury and comfort await at the world's most exclusive hotels and resorts. Start your journey today.</p>
+
+            <form className='bg-white text-gray-500 rounded-lg mt-8 px-6 py-4  flex flex-col md:flex-row max-md:items-start gap-4 max-md:mx-auto'>
+                <div>
+                    <div className='flex items-center gap-2'>
+                        <CalendarOutlined className='h-4' />
+                        <label htmlFor="destinationInput">Destination</label>
+                    </div>
+                    <input list='destinations' id="destinationInput" type="text" className=" rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none" placeholder="Type here" required />
+                    <datalist id='destinations'>
+                        {cities.map((city, index) => (
+                            <option key={index} value={city} />
+                        ))}
+                    </datalist>
+                </div>
+
+                <div>
+                    <div className='flex items-center gap-2'>
+                        <CalendarOutlined className='h-4' />
+                        <label htmlFor="checkIn">Check in</label>
+                    </div>
+                    <input id="checkIn" type="date" className=" rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none" />
+                </div>
+
+                <div>
+                    <div className='flex items-center gap-2'>
+                        <CalendarOutlined className='h-4' />
+                        <label htmlFor="checkOut">Check out</label>
+                    </div>
+                    <input id="checkOut" type="date" className=" rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none" />
+                </div>
+
+                <div className='flex md:flex-col max-md:gap-2 max-md:items-center'>
+                    <label htmlFor="guests">Guests</label>
+                    <input min={1} max={4} id="guests" type="number" className=" rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none  max-w-16" placeholder="0" />
+                </div>
+
+                <button className='flex items-center justify-center gap-1 rounded-md bg-black py-3 px-4 text-white my-auto cursor-pointer max-md:w-full max-md:py-1' >
+
+                    <SearchOutlined />
+                    <span>Search</span>
+                </button>
+            </form>
         </div>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,12 +27,10 @@ const Navbar = () => {
     return (
         <nav className={`fixed top-0 left-0   w-full flex items-center justify-between px-4 md:px-16 lg:px-24 xl:px-32 transition-all duration-500 z-50 ${isScrolled ? "bg-white/80 shadow-md text-gray-700 backdrop-blur-lg py-3 md:py-4" : "py-4 md:py-6"}`}>
 
-            {/* Logo */}
             <Link to='/' >
                 <img src={assets.logo} alt="logo" className={`h-9 ${isScrolled && "invert opacity-80"}`} />
             </Link>
 
-            {/* Desktop Nav */}
             <div className="hidden md:flex items-center gap-4 lg:gap-8">
                 {navLinks.map((link, i) => (
                     <a key={i} href={link.path} className={`group flex flex-col gap-0.5 ${isScrolled ? "text-gray-700" : "text-white"}`}>
@@ -45,7 +43,6 @@ const Navbar = () => {
                 </button>
             </div>
 
-            {/* Desktop Right */}
             <div className="hidden md:flex items-center gap-4">
                 <SearchOutlined alt="search" className={`${isScrolled && 'invert'} h-7 transition-all duration-500 `} />
                 < button className="bg-black text-white px-8 py-2.5 rounded-full ml-4 transition-all duration-500">
@@ -53,12 +50,10 @@ const Navbar = () => {
                 </button>
             </div>
 
-            {/* Mobile Menu Button */}
             <div className="flex items-center gap-3 md:hidden">
                 <MenuOutlined onClick={() => setIsMenuOpen(!isMenuOpen)} alt="" className={`${isScrolled && 'invert'} h-4`} />
             </div>
 
-            {/* Mobile Menu */}
             <div className={`fixed top-0 left-0 w-full h-screen bg-white text-base flex flex-col md:hidden items-center justify-center gap-6 font-medium text-gray-800 transition-all duration-500 ${isMenuOpen ? "translate-x-0" : "-translate-x-full"}`}>
                 <button className="absolute top-4 right-4" onClick={() => setIsMenuOpen(false)}>
                     <CloseOutlined alt="close-menu" className="h-6.5" />
